a379c555dca94f97dfd730ec15e4554079c805fc
Joseph Do <jdoseph02@gmail.com>
2025-09-17 22:23:41 -0400

    fixed date

--- src/components/WhatsNew.js
+++ src/components/WhatsNew.js
@@ -98,7 +98,7 @@ const WhatsNew = () => {
       title: "Added Italy to About Section",
       description: "Added my trip to Italy in the About section with a new episode card.",
       type: "Update",
-      date: "Sept 7, 2025",
+      date: "Sept 17, 2025",
       icon: <FaSync />,
       color: "#fff"
     }
